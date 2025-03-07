13d2affdc9a60fa4fc8e037d6075f90740abfa94
ch0rch <georojas.gs@gmail.com>
2025-03-06 23:52:59 -0300

    asd

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,11 @@
 import TravelStampGenerator from "@/components/travel-stamp-generator"
 import { HeroSection } from "@/components/hero-section"
-import StampDesignConcept from "@/components/stamp-design-concept"
 import { SiteFooter } from "@/components/site-footer"
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-gradient-to-b from-amber-50 to-orange-100">
       <HeroSection />
-
-      <div className="container mx-auto px-4 py-8">
-        <StampDesignConcept />
-      </div>
 
       <div className="container mx-auto px-4 py-12" id="generator">
         <TravelStampGenerator />
@@ -19,6 +14,7 @@ export default function Home() {
     </main>
   )
 }
+
 
 
 
